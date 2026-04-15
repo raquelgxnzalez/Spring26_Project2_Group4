1b0f31fde794632d92f67950103a2f9c87396adc
Raquel Gonzalez <raqueliegonzalez02@gmail.com>
2026-04-15 15:49:44 -0700

    fixed EDUC_A missing code removal

--- scripts/02_data_cleaning.R
+++ scripts/02_data_cleaning.R
@@ -4,6 +4,8 @@ library(tidyverse)
 # Load original dataset
 nhis_data <- read.csv("data/NHIS _Data_2021.csv")
 
+# Task 1: Variable Selection & Missing Value Check
+
 # Select only the required variables
 nhis_subset <- nhis_data %>%
   select(AGEP_A, WEIGHTLBTC_A, HEIGHTTC_A, SEX_A, HISPALLP_A, EDUCP_A, PHSTAT_A, LSATIS4R_A)
@@ -18,12 +20,15 @@ nhis_clean <- nhis_subset %>%
   mutate(WEIGHTLBTC_A = ifelse(WEIGHTLBTC_A >= 996, NA, WEIGHTLBTC_A)) %>%
   # Remove 96-99 from HEIGHTTC_A
   mutate(HEIGHTTC_A = ifelse(HEIGHTTC_A >= 96, NA, HEIGHTTC_A)) %>%
-  # Remove 97-99 from HISPALLP_A
-  mutate(HISPALLP_A = ifelse(HISPALLP_A >= 97, NA, HISPALLP_A)) %>%
-  # Remove 7, 8, 9 from SEX_A, PHSTAT_A, LSATIS4R_A, and EDUCP_A
+  # Remove 97-99 from HISPALLP_A and EDUC_A
   # Use across() to work with multiple variables at once
-  mutate(across(c(SEX_A, PHSTAT_A, LSATIS4R_A, EDUCP_A),
+  mutate(across(c(HISPALLP_A, EDUCP_A),
+                ~ ifelse(.x %in% c(97, 98, 99), NA, .x))) %>%
+  # Remove 7, 8, 9 from SEX_A, PHSTAT_A, and LSATIS4R_A
+  # Use across() to work with multiple variables at once
+  mutate(across(c(SEX_A, PHSTAT_A, LSATIS4R_A),
                 ~ ifelse(.x %in% c(7, 8, 9), NA, .x))) %>%
   # Remove all rows that now have an NA
   drop_na()
-  
+
+# Task 2: Recoding Variables
